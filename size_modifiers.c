c34e3dc26263d3fa28a2e08a127f38e6aeaa197b
Pegasus Epsilon <pegasus@pimpninjas.org>
2020-04-17 10:49:55 -0400

    Make size modifiers work properly, slightly more efficient

--- size_modifiers.c
+++ size_modifiers.c
@@ -12,22 +12,22 @@
 
 void keep_on_screen (BOX *t) {
 	// constrain windows to viewable area
-	t->x = MAX(t->x, 0);
-	t->y = MAX(t->y, 0);
-	if (t->x + t->w > root.width) t->w = root.width - t->x;
-	if (t->y + t->h > root.height) t->h = root.height - t->y;
+	if (t->x < 0) { t->w = t->w + t->x; t->x = 0; }
+	else if (t->x + t->w > root.width) t->w = root.width - t->x;
+	if (t->y < 0) { t->h = t->h + t->y; t->y = 0; }
+	else if (t->y + t->h > root.height) t->h = root.height - t->y;
 }
 void snap_to_edges (BOX *t) {
 	// snap while resizing (window size varies)
 	unsigned snap_x = root.width - t->x;
 	unsigned snap_y = root.height - t->y;
 	if (abs(t->x) < SNAP) { t->w = t->w + t->x; t->x = 0; }
+	else if (abs(snap_x - t->w) < SNAP) t->w = snap_x;
 	if (abs(t->y) < SNAP) { t->h = t->h + t->y; t->y = 0; }
-	if (abs(snap_x - t->w) < SNAP) t->w = snap_x;
-	if (abs(snap_y - t->h) < SNAP) t->h = snap_y;
+	else if (abs(snap_y - t->h) < SNAP) t->h = snap_y;
 }
 void (*size_modifiers[])(BOX *) = {
-	snap_to_edges,
-	keep_on_screen
+	keep_on_screen,
+	snap_to_edges
 };
 unsigned size_modifiers_length = sizeof(size_modifiers) / sizeof(void (*));
