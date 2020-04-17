8f7040e71b78085ad1b1094d4ea8ec05e98c29b2
Pegasus Epsilon <pegasus@pimpninjas.org>
2020-04-17 09:44:02 -0400

    Add headers + include guard

--- events.h
+++ events.h
@@ -1,4 +1,15 @@
+/* events.h - Pegasus Epsilon's Desktop Environment
+** Goal: Less than a megabyte resident
+** (C)opyright 2019-2020 "Pegasus Epsilon" <pegasus@pimpninjas.org>
+** Distribute Unmodified - https://pegasus.pimpninjas.org/license
+*/
+
+#ifndef EVENTS_H
+#define EVENTS_H
+
 char *event_names[] = {
 #define EVENT_EXPANDO(x) [x] = #x,
 #include "expandos.h"
 };
+
+#endif // EVENTS_H
